b79b4a9e5d894365eeb3ea0b118ba0890831dc19
anzboi <daniel.cantos@anz.com>
2020-06-15 11:35:23 +1000

    update spanner ddl to latest to include foreign key constraints

--- examples/spanner_ddl_parser/example.sql
+++ examples/spanner_ddl_parser/example.sql
@@ -17,4 +17,10 @@ CREATE TABLE CustomerHasAccount (
     AccountNum  STRING(23) NOT NULL,
     LegalRole   STRING(10) NOT NULL,
     Permissions ARRAY<STRING(10)>,
+    CONSTRAINT FK_CustomerID FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID),
+    CONSTRAINT FK_AccountNum FOREIGN KEY (AccountNum) REFERENCES Account (AccountNum),
 ) PRIMARY KEY (CustomerID, AccountNum);
+
+ALTER TABLE Customer ADD COLUMN HomePhone STRING(8);
+
+ALTER TABLE Customer DROP COLUMN Email;
